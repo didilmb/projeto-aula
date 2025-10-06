1420aba91bc646a2e4c61c3df91b9dab14621cc5
didilmb <dianalmb8@gmail.com>
2025-10-06 18:59:06 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,19 @@
 import streamlit as st
 st.write("Alô mundo")
 
+import streamlit as st
+
+st.write("Alô mundo")
+
 nome = st.text_input("Digite o seu nome:")
 if nome:
-  st.write(nome.upper())
+    st.write(nome.upper())
+
 st.write("Vamos brincar de adivinhar fatos!")
+
 time = st.text_input("Qual é o melhor time do Brasil?")
-if time = "flamengo".upper
-   st.write("Parabéns! Você acertou! SRN")
-else
-st.write("Errado! Tente novamente")
+
+if time.lower() == "flamengo":
+    st.write("Parabéns! Você acertou! SRN")
+elif time:  
+    st.write("Errado!")
