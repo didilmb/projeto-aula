6e991a67d482a33854f19172e9db2e172acde07b
didilmb <dianalmb8@gmail.com>
2025-10-06 18:48:22 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,3 +4,9 @@ st.write("Alô mundo")
 nome = st.text_input("Digite o seu nome:")
 if nome:
   st.write(nome.upper())
+st.write("Vamos brincar de adivinhar fatos!")
+time = st.text_input("Qual é o melhor time do Brasil?")
+if time = "flamengo".upper
+   st.write("Parabéns! Você acertou! SRN")
+else
+st.write("Errado! Tente novamente")
