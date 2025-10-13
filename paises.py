7bb41a062e586e03166debc67a5a7a6ab1a19532
didilmb <dianalmb8@gmail.com>
2025-10-13 15:08:22 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -3,4 +3,9 @@ import streamlit as st
 import ploty.express as px
 dataset=pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 st.dataframe(dataset)
-fig=px.red
+fig = px.choropleth(dataset,
+                    locations=dataset['iso3'],
+                    color=dataset['name'],
+                    hover_name=dataset['name'])
+fig.update_layout(title='Mapa Coroplético dos Países',
+                  geo_scope='world')
