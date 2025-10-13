8c01f7c3ddcfd4dfaa76b0823de0bd8183b89b26
didilmb <dianalmb8@gmail.com>
2025-10-13 15:19:17 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import ploty.express as px
+import plotly.express as px
 dataset=pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 st.dataframe(dataset)
 fig = px.choropleth(dataset,
